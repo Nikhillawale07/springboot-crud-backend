50841977313339d188ba8e2c30695cce4788f296
Nikhil <nikhillawale2911@gmail.com>
2025-08-16 22:50:47 +0530

    springboot project

---
+++ sb2/src/main/java/com/demo/dao/StudentDao.java
@@ -0,0 +1,16 @@
+package com.demo.dao;
+
+import java.util.List;
+
+import com.demo.model.Student;
+
+public interface StudentDao {
+	int insertStudent(Student s);
+	int deleteStudentById(int id);
+	Student findStudentById(int id);
+	
+	List<Student> findAllStudent();
+	
+	int updateStudent(Student s);
+
+}
---
+++ sb2/src/main/java/com/demo/model/StudentRowMapper.java
@@ -0,0 +1,22 @@
+package com.demo.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class StudentRowMapper implements RowMapper<Student>{
+
+	@Override
+	public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+	Student s1= new Student();
+	s1.setId(rs.getInt("id"));
+	s1.setName(rs.getString("name"));
+	s1.setCity(rs.getString("city"));
+	s1.setPercentage(rs.getDouble("percentage"));
+	return s1;
+	
+	
+	}
+
+}
